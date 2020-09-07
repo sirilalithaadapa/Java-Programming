e156eb2837e072ec0e2db478c03cd0950c22d327
Siri Lalitha <sirilalitha9088@gmail.com>
2020-09-07 23:11:58 +0530

    Create HelloJava.java

---
+++ practise-pro/class/HelloJava.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+public class Main{
+        public static void main(String args[]){
+        
+                // Write your code here
+                Scanner in = new Scanner(System.in);
+                String name = in.nextLine();
+                System.out.println("Hi, Welcome " + name + " to the world of JAVA");
+                
+        }
+}

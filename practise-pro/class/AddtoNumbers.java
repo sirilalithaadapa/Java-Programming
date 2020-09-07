23381f5a50f98fc2e0e55c356a6eb7b01620b400
Siri Lalitha <sirilalitha9088@gmail.com>
2020-09-07 23:10:06 +0530

    Create AddtoNumbers.java

---
+++ practise-pro/class/AddtoNumbers.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+public class AddTwoNums{
+           public static void main(String args[]){
+                      // Write your code here
+                Scanner in = new Scanner(System.in);
+                int a = in.nextInt();
+                int b = in.nextInt();
+
+                System.out.println(a+b);
+           }
+}

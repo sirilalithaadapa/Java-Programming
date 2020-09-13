d70796a76f7bbeed14387893a031fd3fc921040f
Siri Lalitha <sirilalitha9088@gmail.com>
2020-09-13 18:06:31 +0530

    Update InheritanceDemo.java

--- practise-pro/ClassWork/InheritanceDemo.java
+++ practise-pro/ClassWork/InheritanceDemo.java
@@ -3,15 +3,15 @@ public class InheritanceDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Student stu1 = new Student(501,"VVIT","CSE");
-		stu1.setPersonName("Sriharsha");
-		stu1.setGender('M');
+		Student stu1 = new Student(5L4,"VVIT","CSE");
+		stu1.setPersonName("Siri");
+		stu1.setGender('F');
 		stu1.play();
 		stu1.learn();
 		stu1.eat();
 		stu1.sleep();
 		
-		Employee empl1  = new Employee("abcdef", 'F', 123, " ABC", "XYZ");
+		Employee empl1  = new Employee("Vaseem", 'F', 123, " AB", "Shaik");
 		empl1.work();
 		empl1.eat();
 		empl1.learn();

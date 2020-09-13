8ce553011fe96b9b422c64b08aec052978fc9291
Siri Lalitha <sirilalitha9088@gmail.com>
2020-09-13 17:57:41 +0530

    Create DataTypes.java

---
+++ practise-pro/ClassWork/DataTypes.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class DataTypes{
+	public static void main(String args[]){
+		int integerValue = 25;
+		float floatValue ;
+		byte byteValue ;
+		double doubleValue ;
+		boolean booleanValue ;
+		short shortValue ;
+		long longValue ;
+		char charValue ;
+		System.out.println(integerValue);
+		System.out.println(byteValue);
+		System.out.println(shortValue);
+		System.out.println(longValue);
+		System.out.println(doubleValue);
+		System.out.println(floatValue);
+		System.out.println(charValue);
+		System.out.println(booleanValue);
+	}
+}

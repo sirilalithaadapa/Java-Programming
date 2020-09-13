f233696f68b2cceb658b29895e84b1bbb80f038a
Siri Lalitha <sirilalitha9088@gmail.com>
2020-09-13 17:58:43 +0530

    Create Demo.java

---
+++ practise-pro/ClassWork/Demo.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Demo{
+	public static void main(String args[]){
+		int a = 10;
+		System.out.println("Hello World");
+		System.out.println("Hi");
+		System.out.print("Welcome To");
+		System.out.print("Java Programming");
+		System.out.printf("a = %d",a);
+	}
+}	

ca7144c64c5e697d5b7d49d5896c978d3f733979
Siri Lalitha <sirilalitha9088@gmail.com>
2020-09-14 22:20:43 +0530

    Create TicTacToeBoard.java

---
+++ practise-pro/Workouts/TicTacToeBoard.java
@@ -0,0 +1,13 @@
+//TicTacToeBoard.java - Siri Lalitha 
+
+class TicTacToeBoard {
+	public static void main(String[] args) {
+		System.out.println("+---+---+---+");
+		System.out.println("|   |   |   |");
+		System.out.println("+---+---+---+");
+		System.out.println("|   |   |   |");
+		System.out.println("+---+---+---+");
+		System.out.println("|   |   |   |");
+		System.out.println("+---+---+---+");
+	}
+}

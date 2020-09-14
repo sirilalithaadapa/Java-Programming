6f08f3e7be6b780cf931687bd09a01fca235da36
Siri Lalitha <sirilalitha9088@gmail.com>
2020-09-14 22:27:51 +0530

    Create CardDescription.java

---
+++ practise-pro/Workouts/Card/CardDescription.java
@@ -0,0 +1,12 @@
+//CardDescription.java - Siri Lalitha 
+
+import java.util.*;
+
+public class CardDescription
+{
+	public static void main(String[] args)
+	{
+		Card c1 = new Card("KS");
+		System.out.println("Your card is " + c1.getDescription());
+	}
+}

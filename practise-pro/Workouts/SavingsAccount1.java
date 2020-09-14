9854643c550cc1a21d6c481e929211a58653a84d
Siri Lalitha <sirilalitha9088@gmail.com>
2020-09-14 22:15:47 +0530

    Create SavingsAccount1.java

---
+++ practise-pro/Workouts/SavingsAccount1.java
@@ -0,0 +1,40 @@
+//SavingsAccount.java - Siri Lalitha 
+
+public class SavingsAccount
+{
+	private double balance;
+	private double interest;
+
+	public SavingsAccount()
+	{
+		balance = 0;
+		interest = 0;
+	}
+
+	public SavingsAccount(double initialBalance, double initialInterest)
+	{
+		balance = initialBalance;
+		interest = initialInterest;
+	}
+
+	public void deposit(double amount)
+	{
+		balance = balance + amount;
+	}
+
+	public void withdraw(double amount)
+	{
+		balance = balance - amount;
+	}
+
+	public void addInterest()
+	{
+		balance = balance + balance * interest;
+	}
+
+	public double getBalance()
+	{
+		return balance;
+	}
+
+}

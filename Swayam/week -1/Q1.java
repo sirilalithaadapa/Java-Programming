d3159bbd4baa797c14ac3c63346aa44027e92eff
Siri Lalitha <sirilalitha9088@gmail.com>
2020-09-25 20:07:58 +0530

    Update Q1.java

--- Swayam/week -1/Q1.java
+++ Swayam/week -1/Q1.java
@@ -1,6 +1,6 @@
 //Complete the code segment to find the perimeter and area of a circle given a value of radius.
-You should use Math.PI constant in your program.If radius is zero or less than zero then print 
-" please enter non zero positive number ".
+//You should use Math.PI constant in your program.If radius is zero or less than zero then print 
+//" please enter non zero positive number ".
 
 import java.util.Scanner;  
 public class Exercise1_1 {

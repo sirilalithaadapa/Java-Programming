4cbfa90b78c14d45bfa2982b05aff1096080fe86
Siri Lalitha <sirilalitha9088@gmail.com>
2020-10-18 15:22:17 +0530

    Create RationalNumber.java

---
+++ Experiments/RationalNumber.java
@@ -0,0 +1,35 @@
+package Experiments;
+
+import java.util.Scanner;
+
+// @SIRI LALITHA 
+public class RationalNumber {
+	public static int gcd(int n1, int n2) {
+		
+		// Always set both numbers to positive 
+		n1 = (n1 > 0) ? n1 : -n1;
+		n2 = (n2 > 0) ? n2 : -n2;
+		
+		while (n1 != n2) {
+			if (n1 > n2)
+				n1 -= n2;
+			else
+				n2 -= n1;
+		}
+		
+		return n1;
+	}
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int n1 = in.nextInt();
+		int n2 = in.nextInt();
+		
+		int d = gcd(n1, n2);
+		
+		int dn1 = n1 / d;
+		int dn2 = n2 / d;
+		
+		System.out.println(n1 + " / " + n2 + " Should be represented as " + dn1 + " / " + dn2);
+	}
+}

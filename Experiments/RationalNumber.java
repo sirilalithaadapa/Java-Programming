d36021f87d6f844324dc00b3c288e0374c2b8fae
Siri Lalitha <sirilalitha9088@gmail.com>
2021-02-16 17:45:35 +0530

    Update RationalNumber.java

--- Experiments/RationalNumber.java
+++ Experiments/RationalNumber.java
@@ -2,7 +2,6 @@ package Experiments;
 
 import java.util.Scanner;
 
-//@SIRI LALITHA 
 public class RationalNumber {
 	public static int gcd(int n1, int n2) {
 		
@@ -19,26 +18,38 @@ public class RationalNumber {
 		
 		return n1;
 	}
+	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
+		/**
+		taking input 2 numbers
+		*/
 		int n1 = in.nextInt();
 		int n2 = in.nextInt();
 		
+		/**
+		calculating GCD of two numbers
+		*/
 		int d = gcd(n1, n2);
 		
+		/**
+		Divide the two numbers with their GCD
+		*/
 		int dn1 = n1 / d;
 		int dn2 = n2 / d;
 		
 		System.out.println(n1 + " / " + n2 + " Should be represented as " + dn1 + " / " + dn2);
+		
+		/**
+		
+		Input:
+		500
+		1000
+		
+		Output:
+		50 / 100 Should be represented as 1 / 2
+		
+		*/
 	}
 }
-
-
-
-
-
-
-
-
-
